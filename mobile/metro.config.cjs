55625509ecc8730b3855dddce721bce2cb277d18
Preben Olsen <prebenolsen90@hotmail.com>
2026-04-02 16:12:44 +0200

    changes to app fix

--- mobile/metro.config.cjs
+++ mobile/metro.config.cjs
@@ -3,6 +3,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '..');
+const workspaceNodeModules = path.resolve(workspaceRoot, 'node_modules');
+
+const escapeRegExp = (value) => value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
 const config = getDefaultConfig(projectRoot);
 
@@ -11,10 +14,22 @@ config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
 ];
-config.resolver.disableHierarchicalLookup = true;
+config.resolver.disableHierarchicalLookup = false;
 config.resolver.extraNodeModules = {
   react: path.resolve(projectRoot, 'node_modules/react'),
   'react-native': path.resolve(projectRoot, 'node_modules/react-native'),
+  'react-dom': path.resolve(projectRoot, 'node_modules/react-dom'),
 };
+config.resolver.blockList = [
+  new RegExp(
+    `^${escapeRegExp(path.join(workspaceNodeModules, 'react'))}([\\\\/].*)?$`
+  ),
+  new RegExp(
+    `^${escapeRegExp(path.join(workspaceNodeModules, 'react-native'))}([\\\\/].*)?$`
+  ),
+  new RegExp(
+    `^${escapeRegExp(path.join(workspaceNodeModules, 'react-dom'))}([\\\\/].*)?$`
+  ),
+];
 
 module.exports = config;
